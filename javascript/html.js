ef58889ae49dfe5cc2962811f0c25d50413792d6
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-03-19 22:27:03 +0100

    built and updated hero section

--- javascript/html.js
+++ javascript/html.js
@@ -23,13 +23,50 @@ const getHeroSection = (lang) => {
                     </a>
                 </div>
             </div>
+
+            <div class="side-bar side-left">
+                <div class="scroll-down">
+                    <img src="icons/arrow-down.svg" alt="Arrow-down" class="arrow-1">
+                    <img src="icons/arrow-down.svg" alt="Arrow-down" class="arrow-2">
+                </div>
+            <div class="v-line-left"></div></div>
+
+            <div class="side-bar side-right">
+                <a href="mailto:walgi@email.de" class="side-email">walgi@email.de</a>
+                    <div class="side-socials">
+                        <a href="#"><img src="icons/github1.svg" alt="GitHub"></a>
+                        <a href="#"><img src="icons/linkedin1.svg" alt="LinkedIn"></a>
+                    </div>
+                <div class="v-line-right"></div>
+            </div>
+
+            <div class="billboard-container"> <div class="billboard-ticker"> <div class="billboard-track">
+                <span>Available for remote work</span>
+                <span class="sep">•</span>
+                <span>Frontend Developer</span>
+                <span class="sep">•</span>
+                <span>Based in Dortmund</span>
+                <span class="sep">•</span>
+                <span>Open to work</span>
+
+                <span class="sep">•</span>
+                <span>Available for remote work</span>
+                <span class="sep">•</span>
+                <span>Frontend Developer</span>
+                <span class="sep">•</span>
+                <span>Based in Dortmund</span>
+                <span class="sep">•</span>
+                <span>Open to work</span>
+            </div>
+            </div>
+            </div>
         </div>
         `;
 };
 
 const getAboutSection = (lang) => {
   return `
-        <section id="about">
+        <section class="about-section" id="about">
             <h2>Über mich</h2>
             <p>Ich bin ein motivierter Frontend-Entwickler...</p>
         </section>
