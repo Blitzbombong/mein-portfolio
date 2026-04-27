aaf7fd25f167eb745cd79824469064d05e3dcfb9
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-04-27 22:30:29 +0200

    Fonts have been locally integrated, and the legal notice and privacy policy have been updated

--- javascript/html/legal.js
+++ javascript/html/legal.js
@@ -6,7 +6,7 @@
  */
 export function getLegalSection(lang) {
   return `
-    <section id="legal-view" class="legal-section" >
+    <section id="legal-view" class="legal-section">
         <div class="legal-content">
             <button id="close-legal" class="back-button">
                 <img src="icons/arrow-left.svg" alt="Close Legal View" class="back-icon" />
@@ -14,49 +14,43 @@ export function getLegalSection(lang) {
             <h1 class="legal-title">${lang.legal.title}</h1>
             
             <div class="legal-block">
-                <h3>${lang.legal.imprint}</h3>
+                <h3>${lang.legal.imprintTitle}</h3>
                 <ul>
                     <li>Waldemar Giesbrecht</li>
                     <li>Göllenkamp 35</li>
-                    <li>44357 Dortmund, Germany</li>
+                    <li>44357 Dortmund</li>
                 </ul>
             </div>
 
             <div class="legal-block">
-                <h3>${lang.legal.exploringTitle}</h3>
+                <h3>${lang.legal.contactTitle}</h3>
                 <p>Email: winstar1@gmx.net</p>
             </div>
 
+            <h1 class="legal-title">${lang.legal.privacyTitle}</h1>
+
             <div class="legal-block">
-                <h3>${lang.legal.acceptanceTitle}</h3>
-                <p>${lang.legal.acceptanceText}</p>
+                <h3>${lang.legal.privacyTitle}</h3>
+                <p>${lang.legal.privacyShort}</p>
             </div>
 
             <div class="legal-block">
-                <h3>${lang.legal.scopeTitle}</h3>
-                <p>${lang.legal.scopeText}</p>
-                <p>${lang.legal.ownershipText}</p>
+                <h3>${lang.legal.hostingTitle}</h3>
+                <p>${lang.legal.hostingText}</p>
             </div>
 
             <div class="legal-block">
-                <h3>${lang.legal.rightsTitle}</h3>
-                <p>${lang.legal.rightsText}</p>
+                <h3>${lang.legal.generalTitle}</h3>
+                <p>${lang.legal.generalInfo}</p>
             </div>
 
             <div class="legal-block">
-                <h3>${lang.legal.useTitle}</h3>
-                <p>${lang.legal.useText}</p>
+                <h3>${lang.legal.dataTitle}</h3>
+                <p>${lang.legal.dataCollection}</p>
             </div>
 
             <div class="legal-block">
-                <h3>${lang.legal.disclaimerTitle}</h3>
-                <p>${lang.legal.disclaimerText}</p>
-            </div>
-
-            <div class="legal-block">
-                <h3>${lang.legal.indemnityTitle}</h3>
-                <p>${lang.legal.indemnityText}</p>
-                <p>${lang.legal.contactText}</p>
+                <p class="legal-date">${lang.legal.sourceNote}</p>
                 <p class="legal-date">${lang.legal.dateLabel}</p>
             </div>
 
