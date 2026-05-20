454d806728ac31fab107c59a2d83aae4435e13b1
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-05-20 14:11:39 +0200

    update repository URL

--- javascript/functions/projectsData.js
+++ javascript/functions/projectsData.js
@@ -8,8 +8,8 @@ export const myProjects = [
       en: "An interactive encyclopedia fetching data from the PokeAPI. Users can search for their favorite Pokémon, filter details, and view stats in a clean, high-performance interface.",
       de: "Eine interaktive Enzyklopädie, die Daten von der PokeAPI abruft. Nutzer können nach ihren Lieblings-Pokémon suchen, Details filtern und Statistiken in einer sauberen, performanten Oberfläche einsehen.",
     },
-    github: "https://github.com/Blitzbombong/myPokedex.git",
-    live: "https://blitzbombong.github.io/myPokedex/",
+    github: "https://github.com/W-Giesbrecht/myPokedex.git",
+    live: "https://W-Giesbrecht.github.io/myPokedex/",
   },
   {
     name: "Join",
@@ -20,8 +20,8 @@ export const myProjects = [
       en: "Join is a task management tool based on the Kanban system. It allows creating, assigning, and tracking tasks via drag-and-drop to enhance team productivity",
       de: "Join ist ein Aufgaben-Management-Tool, das auf dem Kanban-System basiert. Es ermöglicht das Erstellen, Zuweisen und Verfolgen von Aufgaben per Drag-and-Drop, um die Team-Produktivität zu steigern.",
     },
-    github: "https://github.com/Blitzbombong/join_app.git",
-    live: "https://blitzbombong.github.io/join_app/",
+    github: "https://github.com/W-Giesbrecht/join_app.git",
+    live: "https://W-Giesbrecht.github.io/join_app/",
   },
   {
     name: "El Polo Loco",
@@ -32,8 +32,8 @@ export const myProjects = [
       en: "A classic jump-and-run game built with object-oriented programming. It combines complex collision detection, animations, and game logic for a smooth user experience.",
       de: "Ein klassisches Jump-and-Run-Spiel, das mit objektorientierter Programmierung entwickelt wurde. Es kombiniert komplexe Kollisionsabfragen, Animationen und Spiellogik für ein flüssiges Erlebnis.",
     },
-    github: "https://github.com/Blitzbombong/2D_Game.git",
-    live: "https://blitzbombong.github.io/2D_Game/",
+    github: "https://github.com/W-Giesbrecht/2D_Game.git",
+    live: "https://W-Giesbrecht.github.io/2D_Game/",
   },
 ];
 
